66b58c8dd98adf5a7f7242e9508e464c9c9cfa59
tsantek <santek.vg@gmail.com>
2019-07-20 12:40:26 -0700

    style message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,21 +1,44 @@
 import React from "react";
 
 const Message = props => {
-  console.log(props);
+  const { message } = props;
+  let msg;
+
+  if (!message.read && !message.starred) {
+    msg = "row message unread";
+  }
+
+  if (message.read && !message.starred) {
+    msg = "row message read";
+  }
+  if (message.starred) {
+    msg = "row message read";
+  }
+
   return (
-    <div className="">
+    <div className={msg}>
       <div className="col-xs-1">
         <div className="row">
           <div className="col-xs-2">
             <input type="checkbox" />
           </div>
           <div className="col-xs-2">
-            <i className="star fa fa-star-o" />
+            {message.starred ? (
+              <i className="star fa fa-star-o" />
+            ) : (
+              <i className="star fa fa-star" />
+            )}
           </div>
         </div>
       </div>
       <div className="col-xs-11">
-        <a href="#">sas</a>
+        {message.labels.length > 0 &&
+          message.labels.map(lable => {
+            return <span class="label label-warning">{lable}</span>;
+          })}
+        <a style={{ cursor: "pointer" }} href="#">
+          {message.subject}
+        </a>
       </div>
     </div>
   );
